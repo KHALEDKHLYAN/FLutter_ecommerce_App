ff7d9df4ccf809d6ba0df39d8b0cd33b005fd03d
KHALEDKHLYAN <khaledkhlyan@gmail.com>
2023-10-26 06:21:59 +0300

    ecommerce App

--- lib/widgets/custom_appbar.dart
+++ lib/widgets/custom_appbar.dart
@@ -1,38 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Zero TO Unicorn',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const HomeScreen(),
-    );
-  }
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return  const Scaffold(
-      appBar: CustomAppBar(),
-      bottomNavigationBar: BottomAppBar(),
-    );
-  }
-}
-//ignore the error
-class CustomAppBar extends StatelessWidget with PreferredSizeWidget{
+class CustomAppBar extends StatelessWidget{
   const CustomAppBar({
     super.key,
   });
@@ -63,6 +31,5 @@ class CustomAppBar extends StatelessWidget with PreferredSizeWidget{
     );
   }
   
-  @override
   Size get preferredSize => const Size.fromHeight(50.0);
 }
